e3f7352e5b1279a949eab56c3877b2d6745cda9c
ATORRENS117 <Torrens-A2@ulster.ac.uk>
2025-07-22 18:23:19 +0100

    created assault rifle with firing and a muzzle flash

--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -6,6 +6,7 @@ public class Gun : MonoBehaviour
     public float range = 100f;
 
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
 
     void Update()
     {
@@ -16,10 +17,18 @@ public class Gun : MonoBehaviour
     }
     void Shoot () 
     {
+        muzzleFlash.Play();
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) 
         {
             Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
         }
     }
 }
