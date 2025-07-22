34c5a9de53c3274c9430fc205582999cba3e908d
ATORRENS117 <Torrens-A2@ulster.ac.uk>
2025-07-22 22:39:53 +0100

    set up the assault rifle to be automatic and have impact when firing

--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -4,14 +4,19 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 60f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
 
+    private float nextTimeToFire = 0f;
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) 
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire) 
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
@@ -29,6 +34,14 @@ public class Gun : MonoBehaviour
             {
                 target.TakeDamage(damage);
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
         }
     }
 }
