f12d7c55780983a22f86572218cb02a04908d086
ATORRENS117 <Torrens-A2@ulster.ac.uk>
2025-07-23 02:45:32 +0100

    have moving enemy

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -9,13 +9,19 @@ public class EnemyController : MonoBehaviour
     NavMeshAgent agent;
     void Start()
     {
+        target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
     
     void Update()
     {
-        
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
     }
 
     void OnDrawGizmosSelected ()
